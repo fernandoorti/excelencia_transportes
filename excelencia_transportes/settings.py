27af02daf4da58b65a5e979e335c43d6ff470050
FERNANDO <ortizferjo1@gmail.com>
2025-02-04 09:45:26 -0600

    Agregar dominio de Render a ALLOWED_HOSTS

--- excelencia_transportes/settings.py
+++ excelencia_transportes/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-r!c+8zd6%*y#wk1wwz$9l-p_4tkktf*mfq3d%bhg0(6#gso95p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.37', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['excelencia-transportes.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
